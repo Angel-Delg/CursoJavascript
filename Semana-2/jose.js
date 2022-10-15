e3c857af3454ef70271c34a37c0179dfbe6d703c
jose raul <joseraulbd845@gmail.com>
2022-10-15 15:53:58 -0600

    jose

---
+++ Semana-2/jose.js
@@ -0,0 +1,32 @@
+const arraypapus = ["string", 234 , true , "mama" ]
+const mimamamemima = ["este es un mensaje del numero:",2233456,"enviado a las 8 pm"]
+const kokitofresa = ["quiero decirle a mis padres que los amo antes de morir el"]
+
+const mate = {
+  numeroclaro: 85152987,
+  numerotigo:3340986,
+  modosupersaiyan: 4,
+     
+}
+
+
+const papa = {
+ Name:"jose",
+ surname: "pepa",
+ age: 23
+
+}
+
+const alumno = {
+  notadeciencias: 66,
+  notademates: 55,
+  notadeliteratura: 89,
+
+}
+
+console.log(arraypapus)
+console.log(mimamamemima)
+console.log(papa)
+console.log(alumno)
+console.log(mate)
+console.log(kokitofresa)
